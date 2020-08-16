3d92eb2aecf095ea3b98a44f0c5e39fc868697ac
gui3 <gui.silvent@gmail.com>
2020-08-16 14:33:32 +0200

    I N I T I A L get code from another repository

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # rrrange
+
 create range objects and work with sets theory (inclusion, overlapping, union and so on)
+
+## import
+
+works with browser (ES6+) and Nodejs
+
+in browser, include the rrrange.js file
+
+in node :
+
+```js
+const rrrange = require('rrrange')
+```
+
+## use
+
+rrrange let you create range objects, storing only the boundaries.
+You can then do a selection of operations, such as
+
+- test if some value is in the range
+- test if the range overlaps another range,
+- get the union of 2 ranges
+- ...
+- create arrays from range by specifying a step
+
+by default ranges includes boundaries
+
+```js
+const range = rrrange(2,8)
+const range2 = new rrrange.Range(2,8) // similar
+
+range.includes(5.2) // true
+range.includes(0) // false
+
+range.overlaps(rrrange(4,6)) // true
+
+range.intersects(rrrange(-2,5)) // true
+
+range.reverse() // returns Range(8,2)
+
+range.excludeStart() // returns an open range on 2
+
+range.by(2) // [2,4,6,8]
+range.excludeStart().exludeEnd().by(2) // [4,6]
+```
---
+++ rrrange.js
@@ -0,0 +1,112 @@
+//! frum
+// module template from moment.js
+
+(function (global, factory) {
+  let define
+  typeof exports === 'object' && typeof module !== 'undefined'
+    ? module.exports = factory()
+    : typeof define === 'function' && define.amd
+      ? define(factory)
+      : global.frum = factory()
+}(this, function () { // ----------------------------
+  function assert (condition, errorMessage = 'error') {
+    if (!condition) {
+      throw new Error('frum.js: ' + errorMessage)
+    }
+  }
+
+  function Interval (start, stop) {
+    assert(
+      typeof start === typeof stop,
+      'invalid Interval parameters: ' + start + ', ' + stop
+    )
+    this.start = start
+    this.stop = stop
+    return this
+  }
+
+  // internal functions _____________
+  Interval.prototype.toString = function () {
+    // return 'frum.js.Interval('+this.start+','+this.stop+')'
+    return 'Interval ' +
+      '[' +
+      this.start +
+      '->' +
+      this.stop +
+      ']'
+  }
+
+  Interval.prototype.reversed = function () {
+    return this.start > this.stop
+  }
+  Interval.prototype.low = function () {
+    return this.reversed() ? this.stop : this.start
+  }
+  Interval.prototype.up = function () {
+    return this.reversed() ? this.start : this.stop
+  }
+
+  Interval.prototype.length = function () {
+    return this.up - this.low
+  }
+
+  Interval.prototype.by = function (step = 1, mappingCallback) {
+    // loops through the range and returns an Array
+    assert(
+      step < 0 || step > 0,
+      'parameter step must be a non zero number (' + step + ')'
+    )
+    if (step < 0) {
+      step = -step
+    }
+    if (typeof mappingCallback !== 'function') {
+      mappingCallback = function (x) { return x }
+    }
+    var array = []
+    var mapped = false
+    var sense = this.reversed() ? -1 : 1
+    for (var i = this.start;
+      sense * i <= sense * this.stop;
+      i += sense * step
+    ) {
+      var v = mappingCallback(i)
+      array.push(v)
+      mapped = v === undefined ? mapped : true
+    }
+    if (mapped) { return array }
+  }
+
+  Interval.prototype.contains = function (set) {
+    assert(
+      typeof set === typeof this.start ||
+      set instanceof Interval,
+      'invalid set parameter for .contains'
+    )
+    if (set instanceof Interval) {
+      return this.low() <= set.low() && this.up() >= set.up()
+    } else {
+      return set <= this.up() && set >= this.low()
+    }
+  }
+
+  Interval.prototype.intersects = function (set) {
+    assert(
+      typeof set === typeof this.start ||
+      set instanceof Interval,
+      'invalid set parameter for .contains'
+    )
+    if (set instanceof Interval) {
+      return this.up() >= set.low() && this.low() <= set.up()
+    } else {
+      return set <= this.up() && set >= this.low()
+    }
+  }
+
+  function rrrange (n) {
+    return new Range(n)
+  }
+  frum.FrumNumber = FrumNumber
+  frum.Interval = Interval
+  // ------------------------------------------------
+  return frum
+}))
